218de17733f2e69c4810c41907f1803e23dfbaed
Jagpat Singh <jsingh14192@gmail.com>
2021-04-29 23:37:06 +0530

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,30 +1,35 @@
-let map;
-        async function initMap() {
-        const mapData = await getGeoLocation();
-        map = new google.maps.Map(document.getElementById("map"), {
-            center: { lat: 28.5704192, lng: 77.1915776 },
-            zoom: 8,
-        });
-        }
 
+            let lat, long;
+            const btn = document.getElementById('btn');
+            btn.addEventListener('click', async event =>{    
+                const book = document.getElementById('book').value;
+                const data = {lat, long, book}
+                const options = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                    };
+                    const response = await fetch('/api', options);
+                    const json = await response.json();
+                    console.log(json);
+            });
         
-        async function getGeoLocation(){
-                if('geolocation' in navigator) {
+            if('geolocation' in navigator) {
             // geolocation is available 
             const geo = navigator.geolocation;
-            geo.getCurrentPosition((position) =>{
+            geo.getCurrentPosition(async position =>{
                 console.log(position);
-                const lat = position.coords.latitude;
-                const long = position.coords.longitude;
+                lat = position.coords.latitude;
+                long = position.coords.longitude;
                 document.getElementById('latitude').textContent = lat;
                 document.getElementById('longitude').textContent = long;
-                //console.log(lat, long);
-                return {lat, long}
+                //console.log(lat, long);    
+
             });
 
             } else {
                 // geolocation IS NOT available 
                 console.log(error);
             }
-        }
-        
